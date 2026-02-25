a50591921ed84482c60f395956d744c842bb7c62
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-02-25 13:19:26 +0100

    inserimento font

--- main.js
+++ main.js
@@ -9,19 +9,20 @@ const rowEl = document.querySelector(".row")
 .then(res => res.json())
 .then(data =>{
     console.log(data)
-    data.forEach(photo => {
-        const {title, date, url} = photo
+    data.forEach(scheda => {
+        const {title, date, url} = scheda
 
     const markup = `<div class="col-sm-12 col-md-6 col-lg-4 d-flex justify-content-center">
                    <div class="card p-3 mt-4" style="width: 18rem;">
                    <img src=${url} class="card-img">
                    <img src="./img/pin.svg" class="pin">
                    <div class="card-body">
-                   <p class="card-text">${date}</p>
-                   <p class="card-text">${title}</p>
+                   <p class="card-text data">${date}</p>
+                   <p class="card-text titolo">${title}</p>
                 </div>
                 </div>
                 </div>`;
+
 rowEl.insertAdjacentHTML('beforeend', markup)
     }); 
 })
