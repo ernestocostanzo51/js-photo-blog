18c7d204844b9f69a08bd0e42eb6c2aa63d7b36e
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-02-26 16:05:32 +0100

    milestone 3 (non funzionante)

--- main.js
+++ main.js
@@ -2,11 +2,6 @@
 
 
 const rowEl = document.querySelector(".row")
-let imgEl = document.querySelector(".card-img")
-let imgOverlay = document.querySelector("imgOverlay")
-
-
-
 
 
 
@@ -17,31 +12,33 @@ fetch("https://lanciweb.github.io/demo/api/pictures/")
     data.forEach(scheda => {
         const {title, date, url} = scheda
 
+        
+
+
     const markup = `<div class="col-sm-12 col-md-6 col-lg-4 d-flex justify-content-center">
                    <div class="card p-3 mt-4" style="width: 18rem;">
-                   <a href="#img-full" class="open-link">
+                   <a href="#img-full" class="open-link"
+                   onclick="openOverlay"('${url}')>
                    <img src=${url} class="card-img">
                    </a>
                    <img src="./img/pin.svg" class="pin">
                    <div class="card-body">
                    <p class="card-text data">${date}</p>
                    <p class="card-text titolo">${title}</p>
-                   
                 </div>
                 </div>
-                </div>`;
+                </div>
+                `;
 
-
-
+                openOverlay()
 rowEl.insertAdjacentHTML('beforeend', markup)
-
-
     }); 
 })
 
-addEventListener('click', function(){
-
-})
+function openOverlay(url){
+        const imgOverlay = document.querySelector(".imgOverlay")
+        imgOverlay.src = url
+    }
 
 
 
